28e4587429f194064eceeadc990f277478bb590a
ray-zard <ray-zard@mail.ru>
2017-08-15 19:35:15 +0400

    Доработать анимацию карточек

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,91 +11,16 @@
 
 // $(document).foundation();
 
-// Set the date we're counting down to
-var countDownDate = new Date("Aug 20, 2017 13:00:00 GMT+4").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// const maxHeightElem = document.getElementsByClassName('block-goods');
 
-  // Get todays date and time
-  var now = new Date().getTime();
+// const cardList = new Array(maxHeightElem)
 
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	
-  // Display the result in the element with id="demo"
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-	document.getElementById("minutes").innerHTML = minutes;
-	
-	// alert(days.length);
-
-	// if (days < 5) {
-	// 	document.getElementById("days").nextSibling.innerHTML = 'Дня';
-	// } if (days < 2) {
-	// 	document.getElementById("days").nextSibling.innerHTML = 'Днь';
-	// }
-
-	if (days == 21) {
-		document.getElementById("days").nextSibling.innerHTML = 'День';
-	} if (days > 21) {
-		document.getElementById("days").nextSibling.innerHTML = 'Дней';
-	} if (days > 5) {
-		document.getElementById("days").nextSibling.innerHTML = 'Дня';
-	} if (days < 1) {
-		document.getElementById("days").nextSibling.innerHTML = 'День';
-	}
+// cardList.forEach(function(el) {
+// 	alert(el.height);
+// }, this);
 
 
-	if (hours > 21) {
-		document.getElementById("hours").nextSibling.innerHTML = 'Часа';
-	} if (hours == 21) {
-		document.getElementById("hours").nextSibling.innerHTML = 'Час';
-	} if (hours >= 5) {
-		document.getElementById("hours").nextSibling.innerHTML = 'Часов';
-	} if (hours < 1) {
-		document.getElementById("hours").nextSibling.innerHTML = 'Часа';
-	} if (hours < 5) {
-		document.getElementById("hours").nextSibling.innerHTML = 'Час';
-	}
-	   
-
-	if (minutes < 5) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минута';
-	} if (minutes < 2) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минуты';
-	} if (minutes >= 5) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минут';
-	} if (minutes == 21) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минута';
-	} if (minutes > 21) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минуты';
-	} if (minutes >= 25) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минут';
-	} if (minutes == 31) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минута';
-	} if (minutes > 31) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минуты';
-	} if (minutes >= 35) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минут';
-	} if (minutes == 41) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минута';
-	} if (minutes > 41) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минуты';
-	} if (minutes >= 45) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минут';
-	} if (minutes == 51) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минута';
-	} if (minutes < 51) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минуты';
-	} if (minutes >= 55) {
-		document.getElementById("minutes").nextSibling.innerHTML = 'Минут';
-	}
-
-}, 1000);
+// maxHeightElem.forEach(function(i) {
+// 	alert(i);
+// });
