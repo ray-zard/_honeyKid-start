3767d2eb82bef2b19a34ef6ada61f47535836019
ray-zard <ray-zard@mail.ru>
2017-09-06 11:22:34 +0400

    Выпадающие менюшки для навигации

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,6 +7,7 @@ window.$ = $;
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 import './lib/foundation-explicit-pieces';
+import './lib/main-scripts';
 
 $(document).foundation();
 
---
+++ src/assets/js/lib/main-scripts.js
@@ -0,0 +1,4 @@
+// var cards = document.getElementsByClassName('card');
+// [].forEach.call(cards, function(element) {
+// 	Math.max.apply(null, element);
+// });
